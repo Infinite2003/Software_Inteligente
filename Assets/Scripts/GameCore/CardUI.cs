28c19ddc3d201bd3bd1837de1ff9e9f74b451c4f
kmilop <camilo.e.provoste.p@gmail.com>
2026-05-25 03:03:00 -0400

    Revert "Merge branch 'Michel' into Camilo"
    This reverts commit bc14319f0bd204b19578934f9827660af9908251, reversing
    changes made to 58d4115b767866830712db0f33e3fe4cd38870d4.

--- Assets/Scripts/GameCore/CardUI.cs
+++ Assets/Scripts/GameCore/CardUI.cs
@@ -1,36 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class CardUI : MonoBehaviour
 {
-    [Header("Textos de la Carta")]
     [SerializeField] private TextMeshProUGUI nombreTexto;
     [SerializeField] private TextMeshProUGUI hpTexto;
     [SerializeField] private TextMeshProUGUI tipoTexto;
-    [SerializeField] private TextMeshProUGUI idTexto;
-    [SerializeField] private TextMeshProUGUI categoriaTexto;
-    [SerializeField] private TextMeshProUGUI subCategoriaTexto;
-    [SerializeField] private TextMeshProUGUI descripcionTexto;
-    [SerializeField] private TextMeshProUGUI efectoTexto;
-    [SerializeField] private TextMeshProUGUI costeRetiradaTexto;
-
-    [HideInInspector] public TCGPCard cardData;
 
     public void SetData(TCGPCard carta)
     {
-        cardData = carta;
-
-        // Asignación directa de textos pasarle la información limpia
-        if (nombreTexto != null) nombreTexto.text = carta.name;
-        if (hpTexto != null) hpTexto.text = carta.hp.ToString();
-        if (tipoTexto != null) tipoTexto.text = carta.type.ToString();
-        if (idTexto != null) idTexto.text = carta.id;
-
-        if (categoriaTexto != null) categoriaTexto.text = carta.category.ToString();
-        if (subCategoriaTexto != null) subCategoriaTexto.text = carta.sub_category.ToString();
-
-        if (descripcionTexto != null) descripcionTexto.text = carta.description;
-        if (efectoTexto != null) efectoTexto.text = carta.effect;
-        if (costeRetiradaTexto != null) costeRetiradaTexto.text = carta.retreat_cost.ToString();
+        nombreTexto.text = carta.name;
+        hpTexto.text = "HP: " + carta.hp;
+        tipoTexto.text = carta.type.ToString();
     }
 }
